078377d5a18031fd63883200602c104299fa439c
Kim Bui <81892049+thanhkimbui@users.noreply.github.com>
2022-12-20 11:46:46 -0800

    Finish challenge 18

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const { get } = require("lodash");
 
+
+// Load the full build.
+const _ = require("lodash");
+/* // Load the core build.
+// vrsar _ = require('lodash/core');
+// Load the FP build for immutable auto-curried iteratee-first data-last methods.
+var fp = require('lodash/fp');
+
+// Load method categories.
+var array = require('lodash/array');
+var object = require('lodash/fp/object');
+
+// Cherry-pick methods for smaller browserify/rollup/webpack bundles.
+var at = require('lodash/at');
+var curryN = require('lodash/fp/curryN'); */
+
 let posts = [];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -30,16 +46,13 @@ app.get("/", (rootGetReq, rootGetRes) => {
 app.get("/posts/:postName", (rootGetReq, rootGetRes) => {
   let requestedTitle = rootGetReq.params.postName;
 
-  posts.forEach((post) => { 
-    if (post.title === requestedTitle) {
+  posts.forEach((post) => {
+    if (_.lowerCase(post.title) === _.lowerCase(requestedTitle)) {
       console.log("Match Found!");
     } else {
       console.log("No Match Found!");
     }
   });
-  if (rootGetReq.params.postName === "postTitle") {
-    console.log("Match Found!");
-  }
 });
 
 app.get("/about", (aboutGetReq, aboutGetRes) => {
